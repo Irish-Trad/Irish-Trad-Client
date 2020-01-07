7624681d65d83d949f2c36edb2d65ec7e4f2c7e1
itsmarkodowd <godfather_mark@hotmail.com>
2020-01-07 22:06:40 +0000

    updated public folder - favicons and meta tags

--- src/components/Header/Header.styles.jsx
+++ src/components/Header/Header.styles.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledHeader = styled.header`
+export const HeaderContainer = styled.header`
   background-color: #006400;
   color: white;
   text-align: center;
--- src/components/MainContent/MainContent.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { StyledMainContent } from "./MainContent.styles";
-
-const MainContent = () => (
-  <StyledMainContent>This is the main content section</StyledMainContent>
-);
-
-export default MainContent;
