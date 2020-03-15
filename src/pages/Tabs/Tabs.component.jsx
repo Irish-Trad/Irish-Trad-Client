a83b8fbd094740e4a2a8b11647bba023352417b0
itsmarkodowd <16547497+itsmarkodowd@users.noreply.github.com>
2020-03-15 23:38:10 +0000

    initial Tabs page files

---
+++ src/pages/Tabs/Tabs.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { MainContainer, H1, Text } from 'common/StyledComponents'
+
+const Tabs = () => (
+  <MainContainer>
+    <H1>Tabs</H1>
+    <Text>
+      For those who are more familiar with Tablature than standard music
+      notation or ABC notation I have taken a number of tunes from
+      TheSession.org and converted them into both GDAE and GDAD .pdf's
+    </Text>
+    <Text>
+      If you find these helpful in anyway you should consider visiting
+      TheSession.org and even considering donating to the website. It has stood
+      as a beacon for sharing and educating on traditional irish music for many
+      years.
+    </Text>
+    <Text>https://thesession.org/donate</Text>
+  </MainContainer>
+)
+
+export default Tabs
