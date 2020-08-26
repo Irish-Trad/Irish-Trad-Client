a88245c602c2b03aff6279669dd14b5488d513bd
itsmarkodowd <16547497+itsmarkodowd@users.noreply.github.com>
2020-08-26 17:00:40 +0100

    updated TS to 4.0 and other packages

--- src/App.component.tsx
+++ src/App.component.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react'
+import React, { FC, lazy, Suspense } from 'react'
 
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 
@@ -8,28 +8,28 @@ import Loading from 'pages/Loading'
 
 import { AppContainer } from 'App.styles'
 
-const Homepage = lazy(() =>
-  import('pages/Homepage' /* webpackChunkName: 'Homepage' */)
+const Homepage = lazy(
+  () => import('pages/Homepage' /* webpackChunkName: 'Homepage' */)
 )
 
-const NotFound = lazy(() =>
-  import('pages/NotFound' /* webpackChunkName: 'NotFound' */)
+const NotFound = lazy(
+  () => import('pages/NotFound' /* webpackChunkName: 'NotFound' */)
 )
 
-const Chords = lazy(() =>
-  import('pages/Chords' /* webpackChunkName: 'Chords' */)
+const Chords = lazy(
+  () => import('pages/Chords' /* webpackChunkName: 'Chords' */)
 )
 
-const ClefToTab = lazy(() =>
-  import('pages/ClefToTab' /* webpackChunkName: 'ClefToTab' */)
+const ClefToTab = lazy(
+  () => import('pages/ClefToTab' /* webpackChunkName: 'ClefToTab' */)
 )
 
-const Lessons = lazy(() =>
-  import('pages/Lessons' /* webpackChunkName: 'Lessons' */)
+const Lessons = lazy(
+  () => import('pages/Lessons' /* webpackChunkName: 'Lessons' */)
 )
 
-const Terminology = lazy(() =>
-  import('pages/Terminology' /* webpackChunkName: 'Terminology' */)
+const Terminology = lazy(
+  () => import('pages/Terminology' /* webpackChunkName: 'Terminology' */)
 )
 
 const Newsletter = lazy(
@@ -40,27 +40,30 @@ const Community = lazy(
   () => import('pages/Community') /* webpackChunkName: 'Community' */
 )
 
-const HowToReadTab = lazy(() =>
-  import(
-    'pages/Lessons/pages/HowToReadTab' /* webpackChunkName: 'HowToReadTab' */
-  )
+const HowToReadTab = lazy(
+  () =>
+    import(
+      'pages/Lessons/pages/HowToReadTab' /* webpackChunkName: 'HowToReadTab' */
+    )
 )
 
-const HowToString = lazy(() =>
-  import(
-    'pages/Lessons/pages/HowToString' /* webpackChunkName: 'HowToString' */
-  )
+const HowToString = lazy(
+  () =>
+    import(
+      'pages/Lessons/pages/HowToString' /* webpackChunkName: 'HowToString' */
+    )
 )
 
-const HowToTune = lazy(() =>
-  import('pages/Lessons/pages/HowToTune' /* webpackChunkName: 'HowToTune' */)
+const HowToTune = lazy(
+  () =>
+    import('pages/Lessons/pages/HowToTune' /* webpackChunkName: 'HowToTune' */)
 )
 
-const Tablature = lazy(() =>
-  import('pages/Tablature' /* webpackChunkName: 'Tablature' */)
+const Tablature = lazy(
+  () => import('pages/Tablature' /* webpackChunkName: 'Tablature' */)
 )
 
-const App: React.FC<{}> = () => (
+const App: FC = () => (
   <AppContainer>
     <Router basename={`/${process.env.PUBLIC_URL}`}>
       <Header />
