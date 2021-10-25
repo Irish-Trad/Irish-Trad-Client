ca2bfee6c329a488367c1553e2ac564e8fefd964
itsmarkodowd <16547497+itsmarkodowd@users.noreply.github.com>
2021-10-25 20:44:29 +0100

    Fix: CI fix attempt

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,9 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
-  plugins: [
     [
-      '@babel/plugin-transform-runtime',
-      {
-        regenerator: true,
-      },
+      '@babel/preset-env',
+      { targets: { node: 'current' } },
+      '@babel/preset-typescript',
     ],
   ],
 }
