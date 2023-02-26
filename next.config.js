eaa04bd3dcc3c435e77e569e3ef0be88c6526b10
Mark O'Dowd <contact@markodowd.dev>
2023-02-26 15:09:01 +0000

    initial: default next setup

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-// @ts-check
-
-/**
- * @type {import('next').NextConfig}
- **/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'ie'],
-    defaultLocale: 'en'
-  }
 }
 
 module.exports = nextConfig
